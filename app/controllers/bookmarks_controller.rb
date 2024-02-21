8cab797b7ab4a3e171f708bef3910f361c313177
rossella pellizzi <prellizzi@gmail.com>
2024-02-21 18:26:12 +0100

    db seeded, controllers bookmark and list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.build
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.build(bookmark_params)
+
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Clean database
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+# Creating movies
+puts "Creating movies..."
+
+10.times do |movie|
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.paragraphs(number: 2).join("\n"),
+    poster_url: Faker::Internet.url,
+    rating: rand(1.0..10.0).round(1)
+  )
+  movie.save!
+end
+
+puts "Movies created!"
+
+# Creating lists
+puts "Creates lists..."
+
+5.times do |list|
+  list = List.create(
+    name: Faker::Book.title,
+  )
+  list.save!
+end
+
+puts "Lists created!"
+
+# Creating bookmarks
+puts "Creating bookmarks..."
+
+Movie.all.each do |movie|
+  bookmark = Bookmark.create(
+    comment: Faker::Quote.yoda,
+    movie_id: movie.id,
+    list_id: List.all.sample.id
+  )
+  bookmark.save!
+end
+
+puts "Bookmarks created!"
